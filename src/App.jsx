8147997c36c856a1da9c1c81c3f7c86c68ee644a
Derian Paez <derianpaez01@hotmail.com>
2023-02-16 23:48:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-5xl text-green-600">hello world</p>
+    </div>
+  );
+}
+
+export default App;
